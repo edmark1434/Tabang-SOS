2ccadffa87be7c2d067dfee08e07df681ad50a65
Rexshimura <mortpauljpm03@gmail.com>
2025-11-08 19:33:08 +0800

    Emergency Contacts Datasets

---
+++ src/data/emergencyContacts.js
@@ -0,0 +1,94 @@
+export const emergencyContacts = {
+    categories: [
+        {
+            name: "Major Cities & National Hotlines",
+            contacts: [
+                { name: "National Emergency Hotline", number: "911" },
+                { name: "Cebu City Command Center", number: "166 / (032) 262-1424" },
+                { name: "Cebu City DRRMO", number: "0917-839-9896" },
+                { name: "Cebu City Command Center (Cellphone)", number: "0932-537-7770 / 0917-839-9896" },
+                { name: "Cebu City Fire Office", number: "(032) 256-0541 / 42 / 43" },
+                { name: "Mandaue City DRRMO", number: "(032) 420-2868" },
+                { name: "Mandaue City Command Center", number: "(032) 383-1658" },
+                { name: "BFP Mandaue City", number: "(032) 344-4747 / 344-3364" },
+                { name: "Lapu-Lapu City Nerve Command Center", number: "(032) 431-3771" },
+                { name: "BFP Lapu-Lapu City", number: "(032) 340-0252" },
+                { name: "Talisay City DRRMO", number: "(032) 491-4159" },
+                { name: "BFP Talisay City", number: "(032) 272-8277" },
+                { name: "Naga City DRRMO", number: "0916-361-4638 / 0908-415-7220" },
+                { name: "PNP - Naga", number: "(032) 272-1166 / 0998-598-6338" },
+                { name: "BFP - Naga", number: "0969-406-706" },
+                { name: "Philippine Coast Guard (Naga Sub-Station)", number: "0995-357-9982" },
+                { name: "Cebu Province DRRMO", number: "(032) 255-0046" }
+            ]
+        },
+        {
+            name: "Other Essential Numbers",
+            contacts: [
+                { name: "Visayan Electric", number: "0919-056-8326 / (032) 230-8326" },
+                { name: "Philippine Red Cross (Cebu)", number: "0915-583-9829 / (032) 328-9238" },
+                { name: "Emergency Rescue Unit Foundation (ERUF) - Metro Cebu", number: "161" },
+                { name: "Bureau of Fire Protection (General)", number: "160 / (032) 233-9300 / (032) 255-7287" },
+                { name: "BFP (Smart/TNT)", number: "0918-921-0000" }
+            ]
+        },
+        {
+            name: "Cebu Province Municipalities",
+            contacts: [
+                { name: "Alcantara", number: "0919 067 0898 / 0915 901 8000" },
+                { name: "Alcoy", number: "0962 157 3117" },
+                { name: "Aloguinsan", number: "0922 238 8140 / 0926 530 7338" },
+                { name: "Allegria", number: "(032) 476 7604 / 0928 743 1788" },
+                { name: "Argao", number: "(032) 367 7680 / 344 9871 / 0925 464 5906" },
+                { name: "Asturias", number: "0945 553 1713 / 0950 542 6502" },
+                { name: "Badian", number: "0999 986 3435 / (032) 413 0234" },
+                { name: "Balamban", number: "(032) 333 2190 / 0917 310 8010 / 0985 510 2797" },
+                { name: "Bantayan", number: "0910 622 6622 / 0915 848 6678" },
+                { name: "Barili", number: "0918 637 6365 / 0998 598 6361 (PNP)" },
+                { name: "Bogo City", number: "(032) 342 0580 / 0945 685 2435 / 0917 920 4635 / Command Center: 0995 614 6128" },
+                { name: "Boljoon", number: "(032) 482 9300 / 0961 780 1111" },
+                { name: "Borbon", number: "0999 986 6063 / 0998 598 6381 (PNP)" },
+                { name: "Carcar City", number: "0929 444 7161" },
+                { name: "Carmen", number: "(032) 429 2053 / 0934 976 5885" },
+                { name: "Catmon", number: "0981 188 8559 / (032) 326 4223 / 0927 939 2321 (PNP)" },
+                { name: "Compostela", number: "0917 812 6148" },
+                { name: "Consolacion", number: "(032) 236 2003 / 0931 214 9134" },
+                { name: "Cordova", number: "0917 149 8457 / 0917 116 9819" },
+                { name: "Daanbantayan", number: "0926 825 3800 / 0999 889 7792" },
+                { name: "Dalaguete", number: "0931 703 2223" },
+                { name: "Danao City", number: "0917 153 6955 / 0998 598 6379 (PNP)" },
+                { name: "Dumanjug", number: "0998 733 3000 / 0998 598 6358 (PNP)" },
+                { name: "Ginatilan", number: "(032) 401 4165 / 0917 845 5714" },
+                { name: "Liloan", number: "0956 271 1967 / 0923 905 9077" },
+                { name: "Madridejos", number: "0969 316 2221 / 0950 301 9841 / 0916 647 3749" },
+                { name: "Malabuyoc", number: "(032) 477 8079" },
+                { name: "Medellin", number: "(032) 266 6875 / 0932 866 7704 / 0917 623 6135" },
+                { name: "Minglanilla", number: "0943 850 9665" },
+                { name: "Moalboal", number: "0908 813 0370" },
+                { name: "Naga City", number: "0916 361 4638" },
+                { name: "Oslob", number: "0938 941 0419 / 0998 598 6348 (PNP)" },
+                { name: "Pinamungajan", number: "(032) 468 9686 / 0949 802 4194" },
+                { name: "Pilar", number: "0962 378 3605" },
+                { name: "Poro", number: "0995 156 8504" },
+                { name: "Ronda", number: "0970 778 7177" },
+                { name: "Samboan", number: "0996 789 9798 / 0998 598 6350 (PNP)" },
+                { name: "San Fernando", number: "(032) 260 2083 / 0942 638 3201 / 0962 041 7896 (BFP)" },
+                { name: "San Francisco", number: "0929 541 2144" },
+                { name: "Santa Fe", number: "0981 446 7661 / 0927 447 5176" },
+                { name: "Santander", number: "0961 696 8000" },
+                { name: "Sibonga", number: "(032) 234 6603 / 0995 468 5927 / 0961 850 4538" },
+                { name: "Sogod", number: "0906 537 0939 / 0995 873 6046" },
+                { name: "Tabogon", number: "0917 114 0556" },
+                { name: "Tabuelan", number: "0960 828 4513" },
+                { name: "Talisay City", number: "(032) 407 5928 / 0917 811 4078 / 0969 453 6321" },
+                { name: "Toledo City", number: "0956 817 4215 / 0915 642 6842 (PNP)" },
+                { name: "Tuburan", number: "0933 214 1382 / 0917 116 1363 / 0951 558 4283" },
+                { name: "Tudela", number: "0977 764 1563 / 0922 692 4506" }
+            ]
+        }
+    ],
+    credits: {
+        developedBy: "RavenLabs Development",
+        version: "1.2"
+    }
+};
